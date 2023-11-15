3900478fe5cc61971240fbe39ed9478676652db3
Nicolás <148826576+NicoEve@users.noreply.github.com>
2023-11-14 20:07:11 -0500

    Add files via upload

---
+++ _init_.py
@@ -0,0 +1,18 @@
+from binarios import get_ascii, get_binary, get_results
+
+menu = int(input('Menu\n=====\n1. Character\n2. Word\n'))
+
+if menu == 1:
+    char = input('Enter a character: ')
+    word = char
+elif menu == 2:
+    word = input('Enter a word: ')
+else:
+    print('Invalid choice. Exiting.')
+    exit()
+
+print('Results\n========')
+
+results = get_results(word)
+for result in results:
+    print(result)
